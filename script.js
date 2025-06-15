34ddeed1e98905300c6337d790fe7e5021a35044
informatiker <mich22engineer@gmail.com>
2025-06-16 01:29:12 +0200

    Add colorful portfolio homepage

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function connect() {
-  alert("Connection request sent!");
-}
+const contactBtn = document.getElementById("contactBtn");
+
+contactBtn.addEventListener("click", () => {
+  alert("Thanks for reaching out! I'll get back to you soon 😊");
+});
